fd76a93009a94b59a922c75f5466a3a1d88f72e8
jince360 <jinceseb@protonmail.com>
2026-01-31 15:07:00 +0530

    database configured

--- carforge/carforge/settings.py
+++ carforge/carforge/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'carforge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carforge_db',
+        'USER': 'postgres',
+        'PASSWORD': '12159076',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -124,8 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
+STATICFILES_DIRS = []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
