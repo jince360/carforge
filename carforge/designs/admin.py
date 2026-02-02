ecd752a8175c354587356bef9e610d42010d37e2
jince360 <jinceseb@protonmail.com>
2026-02-02 17:17:17 +0530

    Design admin coustomized

--- carforge/designs/admin.py
+++ carforge/designs/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
 # Register your models here.
 @admin.register(Design)
 class DesignAdmin(admin.ModelAdmin):
-    list_display = ("title", "is_active")
+    def thumbnail(self, object):
+        return format_html("<img src='{}' width='40' height='40' style='object-fit: cover; border-radius: 10px;' />", object.image.url)
+    thumbnail.short_description = "Photo"
+    list_display = ("id", "thumbnail","title", "is_active")
+    list_display_links = ("title", "thumbnail",)
+    search_fields = ("title",)
