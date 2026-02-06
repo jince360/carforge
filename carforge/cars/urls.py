9923ad7cba9bf86d82fdada64e5f3c7f80eb3435
jince360 <jinceseb@protonmail.com>
2026-02-06 15:27:27 +0530

    All cars page added

--- carforge/cars/urls.py
+++ carforge/cars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("cars/<int:car_id>/",cars, name="cars")
+    path("cars/<int:car_id>/",cars, name="cars"),
+    path("all_cars", all_cars, name="all_cars"),
 ]
--- carforge/cars/views.py
+++ carforge/cars/views.py
@@ -4,3 +4,7 @@ from .models import *
 def cars(request, car_id):
     selected_car = get_object_or_404(Car, id=car_id)
     return render(request, "cars/cars.html", {"car":selected_car})
+
+def all_cars(request):
+    all_cars = Car.objects.all().order_by("-created_on")
+    return render(request, "cars/all_cars.html", {"all_cars":all_cars})
