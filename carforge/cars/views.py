4e7fdc688212be4b8ea8056d116b4b2fc97dfaa5
jince360 <jinceseb@protonmail.com>
2026-02-06 16:27:59 +0530

    Pagination added

--- carforge/cars/views.py
+++ carforge/cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 # Create your views here.
 def cars(request, car_id):
     selected_car = get_object_or_404(Car, id=car_id)
@@ -7,4 +8,7 @@ def cars(request, car_id):
 
 def all_cars(request):
     all_cars = Car.objects.all().order_by("-created_on")
-    return render(request, "cars/all_cars.html", {"all_cars":all_cars})
+    paginator = Paginator(all_cars, 18)
+    page = request.GET.get("page")
+    paged_car = paginator.get_page(page)
+    return render(request, "cars/all_cars.html", {"all_cars":paged_car})
